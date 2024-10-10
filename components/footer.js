5edfbe8b2b198eccc520324710c44d09dfecc255
Jaime Markkern <jaimemarkkern@gmail.com>
2024-10-10 02:57:43 -0700

    finished project component and applied to digital art projects

--- components/footer.js
+++ components/footer.js
@@ -4,55 +4,55 @@ class Footer extends HTMLElement {
     }
     connectedCallback() {
         this.innerHTML = `
-<style>
-    footer {
-        background-color: #EDEFF2;
-        width: 100%;
-        text-align: center;
-    }
-    footer h3 {
-        color: #152E52;
-        text-decoration: none;
-        float: left;
-        margin: 0 calc(100% - 570px) 16px 16px;
-    }
-    footer p {
-        text-decoration: none;
-    }
-    footer a {
-        text-decoration: none;
-    }
-    @media screen and (max-width: 800px) {
-        footer .column {
+    <style>
+        footer {
+            background-color: #EDEFF2;
             width: 100%;
+            text-align: center;
         }
-    }
-    @media screen and (min-width: 800px) {
-        footer .column {
-            width: calc(33%);
+        footer h3 {
+            color: #152E52;
+            text-decoration: none;
+            float: left;
+            margin: 0 calc(100% - 570px) 16px 16px;
         }
-    }   
-</style>
+        footer p {
+            text-decoration: none;
+        }
+        footer a {
+            text-decoration: none;
+        }
+        @media screen and (max-width: 800px) {
+            footer .column {
+                width: 100%;
+            }
+        }
+        @media screen and (min-width: 800px) {
+            footer .column {
+                width: calc(33%);
+            }
+        }   
+    </style>
 
-<footer>
-    <div class="row">
-    <h3><br>Let's Connect</h3>
-    </div>
-    <div class="row">
-    <div class="column">
-        <p><small><strong>Social</strong></small></p>
-        <p><a href="https://www.linkedin.com/in/jaime-markkern/" target="_blank" style="padding: 0px 0px;">LinkedIn</a></p>
-    </div>
-    <div class="column">
-        <p><small><strong>Email</strong></small></p>
-        <p>jaimemarkkern@gmail.com</p>
-    </div>
-    <div class="column">
-        <p><small>HTML and CSS by me!</small></p>
-        <p style="padding-left: 0px;"><img style="border:0;width:88px;height:31px" src="https://jigsaw.w3.org/css-validator/images/vcss-blue" alt="Valid CSS!"></p>
-    </div>
-    </div>
-</footer>
+    <footer>
+        <div class="row">
+        <h3><br>Let's Connect</h3>
+        </div>
+        <div class="row">
+        <div class="column">
+            <p><small><strong>Social</strong></small></p>
+            <p><a href="https://www.linkedin.com/in/jaime-markkern/" target="_blank" style="padding: 0px 0px;">LinkedIn</a></p>
+        </div>
+        <div class="column">
+            <p><small><strong>Email</strong></small></p>
+            <p>jaimemarkkern@gmail.com</p>
+        </div>
+        <div class="column">
+            <p><small>HTML and CSS by me!</small></p>
+            <p style="padding-left: 0px;"><img style="border:0;width:88px;height:31px" src="https://jigsaw.w3.org/css-validator/images/vcss-blue" alt="Valid CSS!"></p>
+        </div>
+        </div>
+    </footer>
         `;
     }
 }
--- components/project.js
+++ components/project.js
@@ -1,54 +1,39 @@
 class Project extends HTMLElement {
-    constructor() {
+    title = "Title Not Provided"
+    desc = "Description Not Provided"
+    imgsrc = "placeholder.png"
+
+    constructor(title,desc,imgsrc) {
         super();
+        if ( this.getAttribute("title") ) { this.title = this.getAttribute("title"); }
+        if ( this.getAttribute("desc") ) { this.desc = this.getAttribute("desc"); }
+        if ( this.getAttribute("imgsrc") ) { this.imgsrc = this.getAttribute("imgsrc") }
     }
     connectedCallback() {
         this.innerHTML = `
-<style>
-    @media screen and (max-width: 800px) { 
-        .left-project {
-            width: 100%;
-            display: block;
-            margin-left: auto;
-            margin-right: auto;
-        }
-        .right-project {
-            width: 100%;
-        }
-        .right-project img {
-            border-radius: 0px 0px 6px 6px;
-        }
-    }
-    @media screen and (min-width: 800px) {
-        .left-project {
-            width: calc(50% - 64px);
-            padding: 0px 32px 32px 32px;
-            float: left;
-            overflow: hidden;
-        }
-        .right-project {
-            width: calc(50%);
-            float: right;
-        }
-        .right-project img {
-            border-radius: 0px 6px 6px 0px;
-        }
-    }
-</style>
-
-<div class="container">
-    <div class="row">
-        <div class="left-project">
-            <h2>Project Title</h2>
-            <p>This is a project description.</p>
-        </div>
-        <div class="right-project">
-            <img src="./media/placeholder.png">
+    <div class="container">
+        <div class="row">
+            <div class="left-project">
+                <h2>${this.title}</h2>
+                <p>${this.desc}</p>
+            </div>
+            <div class="right-project">
+                <img src="./media/${this.imgsrc}">
+            </div>
         </div>
     </div>
-</div>
         `;
     }
 }
 
 customElements.define('project-component', Project);
+
+/* 
+
+<project-component
+title=""
+desc=""
+imgsrc="">
+</project-component>
+
+*/
