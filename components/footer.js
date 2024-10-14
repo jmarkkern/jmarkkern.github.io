dc08fa450c785e845ecb9014272df30600087dce
Jaime Markkern <jaimemarkkern@gmail.com>
2024-10-14 14:58:40 -0700

    finished redesign

--- components/footer.js
+++ components/footer.js
@@ -9,15 +9,18 @@ class Footer extends HTMLElement {
             background-color: #EDEFF2;
             width: 100%;
             text-align: center;
+            padding-bottom: 36px;
         }
-        footer h3 {
-            color: #152E52;
-            text-decoration: none;
-            float: left;
-            margin: 0 calc(100% - 570px) 16px 16px;
+        footer h4 {
+            font-family: "Open Sans", Verdana, sans-serif;
+            font-size: 14px;
+	        font-weight: bold;
+            color: #151C28;
+            margin: 24px 0px 16px 0px;
         }
         footer p {
             text-decoration: none;
+            margin: 16px 16px 0px 16px;
         }
         footer a {
             text-decoration: none;
@@ -26,10 +29,22 @@ class Footer extends HTMLElement {
             footer .column {
                 width: 100%;
             }
+            footer h3 {
+                color: #152E52;
+                text-decoration: none;
+                float: center;
+                margin: 0 16px 0px 16px;
+            }
         }
         @media screen and (min-width: 800px) {
             footer .column {
                 width: calc(33%);
+            }
+            footer h3 {
+                color: #152E52;
+                text-decoration: none;
+                float: left;
+                margin: 0 calc(100% - 570px) 0px 16px;
             }
         }   
     </style>
@@ -40,16 +55,16 @@ class Footer extends HTMLElement {
         </div>
         <div class="row">
         <div class="column">
-            <p><small><strong>Social</strong></small></p>
+            <h4>Social</h4>
             <p><a href="https://www.linkedin.com/in/jaime-markkern/" target="_blank" style="padding: 0px 0px;">LinkedIn</a></p>
         </div>
         <div class="column">
-            <p><small><strong>Email</strong></small></p>
-            <p>jaimemarkkern@gmail.com</p>
+            <h4>Email</h4>
+            <p><a href="mailto:jaimemarkkern@gmail.com">jaimemarkkern@gmail.com</a></p>
         </div>
         <div class="column">
+            <p style="padding-left: 0px; margin: 24px 0px 16px 0px;"><img style="border:0;width:54px;height:19px" src="https://jigsaw.w3.org/css-validator/images/vcss-blue" alt="Valid CSS!"></p>
             <p><small>HTML and CSS by me!</small></p>
-            <p style="padding-left: 0px;"><img style="border:0;width:88px;height:31px" src="https://jigsaw.w3.org/css-validator/images/vcss-blue" alt="Valid CSS!"></p>
         </div>
         </div>
     </footer>
