7085052ef13392f13948ab774993f27354e942ee
jaime markkern <jaimemarkkern@gmail.com>
2024-12-16 21:24:45 -0800

    add links to some projects

--- components/project.js
+++ components/project.js
@@ -2,15 +2,36 @@ class Project extends HTMLElement {
     title = "Title Not Provided"
     desc = "Description Not Provided"
     imgsrc = "placeholder.png"
+    link = ""
 
-    constructor(title,desc,imgsrc) {
+    constructor(title,desc,imgsrc,link) {
         super();
         if ( this.getAttribute("title") ) { this.title = this.getAttribute("title"); }
         if ( this.getAttribute("desc") ) { this.desc = this.getAttribute("desc"); }
         if ( this.getAttribute("imgsrc") ) { this.imgsrc = this.getAttribute("imgsrc") }
+        if ( this.getAttribute("link") ) { this.link = this.getAttribute("link"); }
     }
     connectedCallback() {
-        this.innerHTML = `
+        if ( this.getAttribute("link") != "" ) {
+            this.innerHTML = `
+
+    <a href="${this.link}" target="_blank">
+        <div class="container">
+            <div class="row">
+                <div class="left-project">
+                    <h2>${this.title}</h2>
+                    <p>${this.desc}</p>
+                </div>
+                <div class="right-project">
+                    <img src="./media/${this.imgsrc}">
+                </div>
+            </div>
+        </div>
+    </a>
+            `;
+        }
+        else {
+            this.innerHTML = `
     <div class="container">
         <div class="row">
             <div class="left-project">
@@ -22,7 +43,8 @@ class Project extends HTMLElement {
             </div>
         </div>
     </div>
-        `;
+            `;
+        }
     }
 }
 
@@ -33,7 +55,8 @@ customElements.define('project-component', Project);
 <project-component
 title=""
 desc=""
-imgsrc="">
+imgsrc=""
+link="">
 </project-component>
 
 */
